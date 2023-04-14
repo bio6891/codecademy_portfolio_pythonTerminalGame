feb75ea07209c6430a58631e51a4c2282321f351
bio6891 <dorien.daneels@hotmail.com>
2023-04-15 00:52:58 +0200

    define project and todos

---
+++ main.py
@@ -0,0 +1,22 @@
+""" codecademy python course
+portfolio project
+create a python terminal game
+I'll create a cycling quiz.
+The user can choose from a predefined list of races
+for which it then has to provide the last 10 winners (in random order).
+A score out of 10 will be given.
+"""
+
+# TO-DO: get the data (source procycling stats)
+
+# TO-DO: initialize the game with some ASCII art
+
+# TO-DO: ask user which race it wants to play with
+
+# TO-DO: ask user for winners of the last 10 years
+
+# TO-DO: check if the user's guess was correct
+
+# TO-DO: keep track of user score
+
+# TO-DO: ask user if it wants to play again (different race)
