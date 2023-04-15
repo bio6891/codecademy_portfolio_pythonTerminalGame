db63819b74595efd44ed3cb3e6d997e7776c7926
bio6891 <dorien.daneels@hotmail.com>
2023-04-15 13:56:10 +0200

    ask user to pick a race

--- main.py
+++ main.py
@@ -17,16 +17,30 @@ import art
 data = pd.read_csv('data.csv', sep=';')
 # print(data)
 
+# TASK: create list that contains unique race names present in data.
+# the user will be able to choose a race from this list.
+unique_races = list(data.race_name.unique())
+
 # OK TASK: initialize the game with some ASCII art
-# print(art.LOGO)
-# print("Welcome to the Cycling Quiz!")
+print(art.LOGO)
+print("-------------------------------------------------------------------------------------------")
+print("Welcome to the Cycling Quiz!")
+print("Instructions: Pick a race and name the last 10 winners (using last names in random order)")
+print("Instructions: Answer 'exit' to quit the game.")
+print("Good Luck!")
+print("-------------------------------------------------------------------------------------------")
 
-# TASK: ask user which race it wants to play with
+GAME_IS_ON = True
+while GAME_IS_ON:
+    # TASK: ask user which race it wants to play with
+    print(f"Available races: {', '.join(unique_races)}")
+    race_choice = input("Pick a race from the ones listed above: ")
+    GAME_IS_ON = False
+    
+    # TASK: ask user for winners of the last 10 years
 
-# TASK: ask user for winners of the last 10 years
+    # TASK: check if the user's guess was correct
 
-# TASK: check if the user's guess was correct
+    # TASK: keep track of user score
 
-# TASK: keep track of user score
-
-# TASK: ask user if it wants to play again (different race)
+    # TASK: ask user if it wants to play again (different race)
